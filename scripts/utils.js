c41fd55065b655e91281b6c0f7f8224d990cb540
tyler hahn <tyler@blizzardpro.com>
2022-08-04 09:42:31 -0500

    potential breaking change

--- scripts/utils.js
+++
@@ -1,7 +0,0 @@
-export const validateEmail = (email) => {
-  //eslint-disable-next-line
-  var re =
-    /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  let testEmail = re.test(String(email).toLowerCase());
-  return testEmail;
-};
--- scripts/warrantycenter.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom";
-import React from "react";
-import WarrantyCenter from "./components/WarrantyCenter";
-
-const rootEl = document.querySelector("#react-embed");
-
-rootEl && ReactDOM.render(<WarrantyCenter />, rootEl);
